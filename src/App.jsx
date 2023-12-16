299b8041a0c6ec01372c0bbcc8db8c0619ce59c4
RobertoGP96 <trolbertogp96@gmail.com>
2023-12-16 11:45:53 -0500

    create working page temp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import './App.css'
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
-import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Store from './pages/Store'
 import Products from './pages/Products'
+import Home from './pages/Home'
 import Page404 from './pages/Page404'
 import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
 import {QueryFiltersContextProvider} from './context/filtersContext.jsx'
-
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
       <PrimeReactProvider>
       <QueryFiltersContextProvider>
         <Routes>
+          <Route path = "/store" element = {<Store/>}/>
+          <Route path = "/contactus" element = {<Contact/>}/>
           <Route path = "/products" element = {<Products/>}/>
           <Route path = "/" element = {<Home/>}/>
           <Route path = "*" element = {<Page404/>}/>
