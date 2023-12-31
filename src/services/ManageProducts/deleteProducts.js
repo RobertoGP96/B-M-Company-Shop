34c252014c9b5ef55cff7e0d0bb7829e326dc679
lazaroah <lazaroah02@gmail.com>
2023-12-31 10:30:45 -0500

    services for manage products added

---
+++ src/services/ManageProducts/deleteProducts.js
@@ -0,0 +1,17 @@
+import {URL_PRODUCTS_OF_SELLER} from "../settings"
+
+export function deleteProducts({products, token}){
+    return(
+        fetch(URL_PRODUCTS_OF_SELLER,{
+            method: 'DELETE',
+            headers: {
+                Authorization: `Token ${token}`,
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({products_to_delete:products})
+        })
+        .then(response => {
+            return response
+        })
+    )
+}
