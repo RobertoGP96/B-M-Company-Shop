f33d2f3862d783ea2dbc8f5e08ac41677f4cb75e
lazaroah <lazaroah02@gmail.com>
2024-02-06 12:01:46 -0500

    fixing discount

--- src/utils/applyDiscount.js
+++ src/utils/applyDiscount.js
@@ -1,7 +1,7 @@
 export function applyDiscount({price, promotion, discount}){
     //descuento normal sin descuento por promocion
     if(discount > 0 && discount != null && promotion == null){
-        return price - discount
+        return price - (price * discount/100)
     }
     //descuento por promocion sin descuento normal
     if(discount == 0 && promotion != null){
@@ -9,7 +9,7 @@ export function applyDiscount({price, promotion, discount}){
     }
     //descuento por promocion y descuento normal
     if(discount > 0 && discount != null && promotion != null){
-        return price - (price * promotion.discount_in_percent/100) - discount
+        return price - (price * promotion.discount_in_percent/100) - (price * discount/100)
     }
     //sin descuento
     return price    
