d8734128803d96b1bd55c7a101fa7c58bc8ff2e3
ArtStyles <fejames07@icloud.com>
2024-02-17 02:28:41 -0500

    merge products-magnament

--- src/settings.js
+++ src/settings.js
@@ -16,3 +16,13 @@ export const URL_ADD_PRODUCTS_PROMOTION = `${BASE_URL}/store/manage-promotions/`
 export const URL_DELETE_PRODUCTS_PROMOTION = `${BASE_URL}/store/manage-promotions/`
 
 export const URL_MAGNAMENT_USERS = `${BASE_URL}/user/managment/`
+
+
+//authentication
+export const LOGIN_URL = `${BASE_URL}/authentication/login/`
+
+//contact info
+export const CONTACT_INFO_URL = `${BASE_URL}/contact_info/`
+
+//user profile
+export const USER_PROFILE_URL = `${BASE_URL}/authentication/user/`
